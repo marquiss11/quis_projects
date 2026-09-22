a4a648c5c3db18cd64e0ee9c7f28a396037639a7
iMamba <marquisscott@iMambas-MacBook-Air.local>
2026-09-22 01:34:19 +0100

    Build employee list and persist employee data

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import EmployeeForm from './components/EmployeeForm';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
-  const [employees, setEmployees] = useState ([]);
+  const [employees, setEmployees] = useState(() => {
+    const savedEmployees = localStorage.getItem('employees');
+    return savedEmployees ? JSON.parse(savedEmployees) : [];
+    });
 
   const saveData = (data) =>{
     localStorage.setItem('employees', JSON.stringify(data));
   };
   const addEmployee = (employee) => {
-    const updatedEmployees = [...employees, employee];
-
-    setEmployees([...employees, employee]);
-    saveData(updatedEmployees);
+  const newEmployee = {
+    ...employee,
+    EmployeeId: Date.now()
   };
+
+  const updatedEmployees = [...employees, newEmployee];
+
+  setEmployees(updatedEmployees);
+  saveData(updatedEmployees);
+};
 
   return (
     <Router>
@@ -22,6 +31,7 @@ function App() {
         <Route path="/">
           <div className="App">
             <EmployeeForm addEmployee={addEmployee} />
+            <EmployeeList employees={employees} />
           </div>
         </Route>
       </Switch>
